13d58510ee2832600fb43c2ccbf44a91adc3d80e
Aditya Jain - www.aadityajain.dev <5467515-aadityajain-dev@users.noreply.gitlab.com>
2022-03-03 22:34:01 +0530

    handeling user auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'wheel-kart';
+  constructor(private _router: Router) {
+    if (localStorage.getItem('wheel-kart-user') === null) {
+      this._router.navigate(['login']);
+    }
+  }
 }
