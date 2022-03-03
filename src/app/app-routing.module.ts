ecc7a10db83eaf1aa991f20960299cc1a05970b5
Aditya Jain - www.aadityajain.dev <5467515-aadityajain-dev@users.noreply.gitlab.com>
2022-03-03 22:33:24 +0530

    adding fall back component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AllProductsComponent } from './component/all-products/all-products.comp
 import { CartComponent } from './component/cart/cart.component';
 import { CheckoutComponent } from './component/checkout/checkout.component';
 import { RegisterComponent } from './component/register/register.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'add-product', component: AddProductComponent },
   { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
+  { path: '**', component: AppComponent },
 ];
 
 @NgModule({
