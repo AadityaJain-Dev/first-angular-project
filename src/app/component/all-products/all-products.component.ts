4dc0bc6bccc0ad2394bb2d67403d7ab1b2291233
Aditya Jain - www.aadityajain.dev <5467515-aadityajain-dev@users.noreply.gitlab.com>
2022-03-05 02:39:38 +0530

    ALL PRODUCTS PAGE

--- src/app/component/all-products/all-products.component.ts
+++ src/app/component/all-products/all-products.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import axios from 'axios';
+import { sharedData } from '../data';
 
 @Component({
   selector: 'app-all-products',
@@ -7,6 +9,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./all-products.component.css'],
 })
 export class AllProductsComponent implements OnInit {
+  allProductsList: any = [];
+
   constructor(private _router: Router) {
     if (localStorage.getItem('wheel-kart-user') === null) {
       this._router.navigate(['login']);
@@ -14,4 +18,27 @@ export class AllProductsComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  getAllProduct = () => {
+    axios
+      .get(sharedData.API_ALL_PRODUCTS_URL)
+      .then((response) => {
+        console.log(response.data);
+        this.allProductsList = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  addToCart = () => {
+    axios
+      .post(sharedData.API_ADD_TO_CART_URL)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 }
